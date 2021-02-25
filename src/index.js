6f6b6677f704126d4c67198c2c4bde6a5679161d
Fernando Luiz Cola <ferlzc@gmail.com>
2021-02-25 18:21:13 -0300

    added struct

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import "./style.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import  particleJS from "particles.js/particles";
 
-const element = document.createElement("h1");
-element.innerHTML = "HELLOOO"
+const element = document.createElement("div");
+element.innerHTML = "HELLOOO FROM JS"
 document.body.appendChild(element)
 
+document.head.appendChild(element)
+
 particlesJS.load('particles', 'assets/particles.json', null);
+
